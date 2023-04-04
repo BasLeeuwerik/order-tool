8ddd9774a940f0baee592aeca3b62451eaa2253d
BasLeeuwerik <bas_leeuwerik@hotmail.com>
2023-04-04 14:25:46 +0200

    added command for temp quotation deletion

--- domain/Quotation/Models/Quotation.php
+++ domain/Quotation/Models/Quotation.php
@@ -10,11 +10,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Quotation extends Model
 {
     use HasFactory;
     use HasUuids;
+    use SoftDeletes;
 
     protected $casts = [
         'status' => QuotationStatusType::class,
