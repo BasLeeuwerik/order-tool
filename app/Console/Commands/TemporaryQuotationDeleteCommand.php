d2a3aee8b1a91e45fcd3e9760eefb9bdb10957fb
BasLeeuwerik <bas_leeuwerik@hotmail.com>
2023-04-04 14:27:02 +0200

    added command for temp quotation deletion

---
+++ app/Console/Commands/TemporaryQuotationDeleteCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Domain\Quotation\Models\Quotation;
+use Illuminate\Console\Command;
+
+class TemporaryQuotationDeleteCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'local:temp';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Used for clearing quotations that have status open for more than 5 days';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): void
+    {
+        $this->line('Deleting temporary quotations');
+
+        $quotations = Quotation::query()
+            ->where('created_at', '<=', now()->subDays(5))
+            ->get();
+
+        foreach ($quotations as $quotation) {
+            $id = $quotation->id;
+
+            $this->info('Deleting temporary quotation: ' . $id);
+            $quotation->delete();
+            $this->info('Deleted temporary quotation: ' . $id);
+        }
+
+        $this->line('Deleted temporary quotations successfully');
+    }
+}
